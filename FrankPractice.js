9833a004fa4f23059b607b25a3116bdc76cf842b
Dani Gomez <daniela.gomezp05@outlook.com>
2020-05-12 18:04:33 -0500

    [Feature] - JS practice

---
+++ FrankPractice.js
@@ -0,0 +1,8 @@
+let name = 'Frank'
+let apellido = 'Quintana'
+let $result = document.querySelector('#result')
+let $pFooter = document.querySelector('.pFooter')
+
+
+$result.innerText = 'Hola soy' + name + 'mi apellido es' + apellido
+
